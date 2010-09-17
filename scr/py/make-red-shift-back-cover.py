b67900e623185514555569e73fe9a83f05f4e31a
trevorbaca <trevorbaca@98c7a01b-27d8-4939-a644-c1b92471d3cd>
2010-09-17 12:44:35 +0000

    Added script to make back cover.

---
+++ scr/py/make-red-shift-back-cover.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import baca
+import os
+
+
+def make_back_covers( ):
+   input_directory = os.path.join(os.environ['REDSHIFT'], 'scr', 'tex')
+   output_directory = os.path.join(os.environ['REDSHIFT'], 'scr', 'pdf')
+   for cover_file_name in ('red-shift-back-cover.tex', ):
+      cover_file_name_stem = cover_file_name[:-4]
+      baca.utilities.render_tex(input_directory, cover_file_name_stem, output_directory)
+
+
+if __name__ == '__main__':
+   os.system('clear')
+   make_back_covers( )

66d60b07cfbae77bf16a766d116f881f7570aa73
trevorbaca <trevorbaca@98c7a01b-27d8-4939-a644-c1b92471d3cd>
2010-11-02 23:39:06 +0000

    Revised score.

--- mus/py/make_red_shift_2.py
+++ mus/py/make_red_shift_2.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 ## rebuilt 2010-09-18
 
 from abjad import *
@@ -12,19 +13,19 @@ assert cons == [10, 19, 20, 23, 24, 26, 27, 29, 30, 33, 37, 40]
 
 cary = [[-2, -12, -10], [18, 8, 7, 17], [15, 25, 21, 4, 11]]
 
-order1 = [p % 12 for p in listtools.flatten(cary)]
+order1 = [p % 12 for p in seqtools.flatten_sequence(cary)]
 assert order1 == [10, 0, 2, 6, 8, 7, 5, 3, 1, 9, 4, 11]
 
-order2 = [p % 12 for p in listtools.flatten(
-   listtools.rotate([listtools.rotate(pt, 1) for pt in cary], -1))]
+order2 = [p % 12 for p in seqtools.flatten_sequence(
+   seqtools.rotate_sequence([seqtools.rotate_sequence(pt, 1) for pt in cary], -1))]
 assert order2 == [5, 6, 8, 7, 11, 3, 1, 9, 4, 2, 10, 0]
 
-order3 = [p % 12 for p in listtools.flatten(
-   listtools.rotate([listtools.rotate(pt, 2) for pt in cary], -2))]
+order3 = [p % 12 for p in seqtools.flatten_sequence(
+   seqtools.rotate_sequence([seqtools.rotate_sequence(pt, 2) for pt in cary], -2))]
 assert order3 == [4, 11, 3, 1, 9, 0, 2, 10, 7, 5, 6, 8]
 
-pitches = listtools.flatten([
-   pitchtools.register_pitch_class_numbers_by_pitch_number_aggregate(o, cons) 
+pitches = seqtools.flatten_sequence([
+   pitchtools.register_chromatic_pitch_class_numbers_by_chromatic_pitch_number_aggregate(o, cons) 
    for o in [order1, order2, order3]])
 
 ## skip unused stuff from make_red_shift_1.py ##
@@ -63,6 +64,13 @@ for definition in violin_tuplet_definitions:
 
 violin_staff = Staff(violin_tuplets)
 violin_staff.name = 'Violin Staff'
+violin_staff.override.beam.positions = (-4, -4)
+violin_staff.override.stem.stem_end_position = -7
+violin_staff.override.tuplet_bracket.staff_padding = 2
+space = schemetools.SchemePair('space', 1)
+minimum_distance = schemetools.SchemePair('minimum-distance', 18)
+vector = schemetools.SchemeVector(space, minimum_distance)
+violin_staff.override.vertical_axis_group.next_staff_spacing = vector
 
 for i, note in enumerate(notetools.iterate_notes_forward_in_expr(violin_staff)):
    note.pitch = pitches[i]
@@ -115,30 +123,93 @@ lh_staff = Staff(lh_tuplets)
 lh_staff.name = 'Piano LH Staff'
 lh_staff[-1:-1] = [Rest((1, 8))]
 
+space = schemetools.SchemePair('space', 1)
+minimum_distance = schemetools.SchemePair('minimum-distance', 16)
+vector = schemetools.SchemeVector(space, minimum_distance)
+rh_staff.override.vertical_axis_group.next_staff_spacing = vector
+
 piano_staff = scoretools.PianoStaff([rh_staff, lh_staff])
 score = Score([violin_staff, piano_staff])
-lily_file = lilyfiletools.make_basic_lily_file(score)
 
-lily_file.default_paper_size = 'letter', 'portrait'
-lily_file.global_staff_size = 16
+for leaves in componenttools.partition_components_once_by_prolated_durations_le_without_overhang(
+   rh_staff.leaves, [Fraction(5, 8), Fraction(5, 8)]):
+   last_leaf = leaves[-1]
+   marktools.LilyPondCommandMark('break', 'closing')(last_leaf)
 
-marktools.TimeSignatureMark(1, 8, target_context = Score)(score)
-marktools.ClefMark('bass')(lh_staff)
-violin_staff.set.instrument_name = markuptools.Markup('Violin')
-violin_staff.set.short_instrument_name = markuptools.Markup('Vn.')
+contexttools.TempoMark(Fraction(1, 8), 32)(score)
+contexttools.TimeSignatureMark(1, 8, target_context = Score)(score)
+contexttools.ClefMark('bass')(lh_staff)
+contexttools.InstrumentMark(r'\hcenter-in #6 Vn.', r'\hcenter-in #6 Vn.')(violin_staff)
+contexttools.InstrumentMark(
+   r'\hcenter-in #6 Pf.', r'\hcenter-in #6 Pf.', target_context = type(piano_staff))(
+   piano_staff)
+first_violin_leaf = leaftools.get_nth_leaf_in_expr(violin_staff, 0)
+markuptools.Markup(r'\dynamic pp \italic { sempre al fino }', 'down')(first_violin_leaf)
+first_violin_leaf.override.text_script.staff_padding = 5
 
-piano_staff.set.instrument_name = markuptools.Markup('Pianoforte')
-piano_staff.set.short_instrument_name = markuptools.Markup('Pf.')
+first_rh_leaf = leaftools.get_nth_leaf_in_expr(rh_staff, 0)
+markuptools.Markup(r'\dynamic pp \italic { sempre al fino }', 'down')(first_rh_leaf)
+first_rh_leaf.override.text_script.staff_padding = 4
 
+score.override.bar_line.transparent = True
+score.override.bar_number.transparent = True
 score.override.beam.breakable = True
-score.override.spacing_spanner.strict_note_spacing = True
+score.override.metronome_mark.padding = 4
+score.override.non_musical_paper_column.line_break_permission = False
+score.override.non_musical_paper_column.page_break_permission = False
 score.override.spacing_spanner.strict_grace_spacing = True
-score.override.uniform_stretching = True
+score.override.spacing_spanner.strict_note_spacing = True
+score.override.spacing_spanner.uniform_stretching = True
+score.override.span_bar.transparent = True
+score.override.time_signature.stencil = False
 score.override.tuplet_bracket.bracket_visibility = True
-
-score.set.proportional_notation_duration = schemetools.SchemeMoment(Fraction(1, 96))
 score.set.auto_beaming = False
+score.set.tuplet_full_length = True
+score.set.proportional_notation_duration = schemetools.SchemeMoment(Fraction(1, 96))
 
-lily_file.layout.contexts.append([r'\Voice', r'\remove Forbid_line_break_engraver'])
+last_leaf = leaftools.get_nth_leaf_in_expr(score, -1)
+marktools.LilyPondCommandMark('bar "||"', 'after')(last_leaf)
+marktools.LilyPondCommandMark("override Score.BarLine #'transparent = ##f", 'after')(last_leaf)
+marktools.LilyPondCommandMark("override Score.SpanBar #'transparent = ##f", 'after')(last_leaf)
+markuptools.Markup(r'\italic { Austin Feb 2006. }', 'down')(last_leaf)
+last_leaf.override.text_script.extra_offset = (-13.5, -7)
+
+lily_file = lilyfiletools.make_basic_lily_file(score)
+lily_file.default_paper_size = 'letter', 'portrait'
+lily_file.global_staff_size = 14
+
+violin_leaf = leaftools.get_nth_leaf_in_expr(violin_staff, 5)
+composer_markup = markuptools.Markup(
+   '\\override #\'(font-name . "Adobe Caslon Pro") '
+   r'\fontsize #3 '
+   r'\line { Trevor Bača }', 'up')
+composer_markup(violin_leaf)
+violin_leaf.override.text_script.extra_offset = (-10, 4)
+
+lily_file.header_block.subtitle = markuptools.Markup(
+   '\\override #\'(font-name . "Adobe Caslon Pro") '
+   r'\raise #-5 '
+   r'\fontsize #5 '
+   r'\line { For Carl on his 60th } '
+   r'\raise #-16 \hspace #0 '
+   )
+lily_file.header_block.title = markuptools.Markup(
+   '\\override #\'(font-name . "Adobe Caslon Pro Bold") '
+   r'\fontsize #8 '
+   r'\line { RED SHIFT HIJINKS }')
+lily_file.layout_block.contexts.append([r'\Voice', r'\remove Forbid_line_break_engraver'])
+lily_file.layout_block.indent = 0
+lily_file.layout_block.ragged_right = True
+space = schemetools.SchemePair('space', 24)
+stretchability = schemetools.SchemePair('stretchability', 0)
+vector = schemetools.SchemeVector(space, stretchability)
+#lily_file.paper_block.after_title_spacing = vector
+space = schemetools.SchemePair('space', 28)
+stretchability = schemetools.SchemePair('stretchability', 0)
+vector = schemetools.SchemeVector(space, stretchability)
+lily_file.paper_block.between_system_spacing = vector
+lily_file.paper_block.left_margin = 20
+lily_file.paper_block.right_margin = 20
+lily_file.paper_block.top_margin = 15
 
 show(lily_file)

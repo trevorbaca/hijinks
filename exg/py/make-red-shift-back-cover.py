5fd349b131f0f5ae4a326ddb420c870faa684ace
trevorbaca <trevorbaca@98c7a01b-27d8-4939-a644-c1b92471d3cd>
2011-08-01 13:04:18 +0000

    Updated score-making script to identify exg in place of scr.

--- exg/py/make-red-shift-back-cover.py
+++ exg/py/make-red-shift-back-cover.py
@@ -5,8 +5,8 @@ import os
 
 
 def make_back_covers( ):
-   input_directory = os.path.join(os.environ['REDSHIFT'], 'scr', 'tex')
-   output_directory = os.path.join(os.environ['REDSHIFT'], 'scr', 'pdf')
+   input_directory = os.path.join(os.environ['REDSHIFT'], 'exg', 'tex')
+   output_directory = os.path.join(os.environ['REDSHIFT'], 'exg', 'pdf')
    for cover_file_name in ('red-shift-back-cover.tex', ):
       cover_file_name_stem = cover_file_name[:-4]
       baca.util.render_tex(input_directory, cover_file_name_stem, output_directory)

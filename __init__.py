6b82bb4969e7623fb2cee9ae89a8ce4f08dc849e
trevorbaca <trevorbaca@98c7a01b-27d8-4939-a644-c1b92471d3cd>
2012-01-19 20:05:40 +0000

    Updated initializer.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import instrumenttools
 from abjad.tools import scoretools
-from baca.scf import TrevorBaca
+
 from collections import OrderedDict
 import mus
 
 
 tags = OrderedDict([
-    ('composer', TrevorBaca()),
+    ('composer', 'Trevor Bača'),
     ('forces_tagline', 'for violin & piano'),
     ('instrumentation', scoretools.InstrumentationSpecifier([
         scoretools.Performer(name='violinist', instruments=[instrumenttools.Violin()]),

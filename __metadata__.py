93819b5d71cc35724150e5a7fe374f6f6a90c127
trevorbaca <trevorbaca@98c7a01b-27d8-4939-a644-c1b92471d3cd>
2014-03-19 16:19:38 +0000

    Rewrote metadata modules.

--- __metadata__.py
+++ __metadata__.py
@@ -6,4 +6,5 @@ metadata = collections.OrderedDict([
     ('composer', 'Trevor Ba\xc4\x8da'),
     ('forces_tagline', 'for violin & piano'),
     ('title', 'Red Shift Hijinks'),
-    ('year_of_completion', 2005)])
+    ('year_of_completion', 2005),
+    ])

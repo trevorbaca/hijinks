15d17c7a321b178d6b97392f319cabdc4076326a
Trevor Bača <trevor.baca@gmail.com>
2016-08-03 17:30:37 -0500

    Rebuilt score.

--- hijinks/__metadata__.py
+++ hijinks/__metadata__.py
@@ -4,8 +4,7 @@ import collections
 
 metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-000-RSH'),
-    ('composer', 'Trevor Ba\xc4\x8da'),
     ('forces_tagline', 'for violin & piano'),
     ('title', 'Red Shift Hijinks'),
-    ('year', 2005),
+    ('year', 2006),
     ])

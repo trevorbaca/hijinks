b6048eb6d02f149f3dd7ceb1f02bae3f99406365
Trevor Bača <trevor.baca@gmail.com>
2016-08-03 10:41:51 -0500

    Updated catalog number.

--- hijinks/__metadata__.py
+++ hijinks/__metadata__.py
@@ -3,6 +3,7 @@ import collections
 
 
 metadata = collections.OrderedDict([
+    ('catalog_number', 'AWN-000-RSH'),
     ('composer', 'Trevor Ba\xc4\x8da'),
     ('forces_tagline', 'for violin & piano'),
     ('title', 'Red Shift Hijinks'),

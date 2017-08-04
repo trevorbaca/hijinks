f892b71c86da9cbe5af41871f8bb59a0d1274507
Trevor Bača <trevor.baca@gmail.com>
2017-08-04 11:39:05 -0500

    Restructured package initializer.

--- hijinks/__init__.py
+++ hijinks/__init__.py
@@ -8,6 +8,6 @@ if not (
     raise ImportError('Requires Python 3.5.')
 del distutils
 del platform
-from hijinks import tools
-from hijinks import materials
+from hijinks.tools import *
+from hijinks.materials import *
 from hijinks import segments

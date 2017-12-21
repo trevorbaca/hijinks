62a84dad2a3fd5a40c3f0aa18ba654813dfd2038
Trevor Bača <trevor.baca@gmail.com>
2017-12-21 10:21:57 -0600

    Cleaning up instrument tags.

--- hijinks/materials/instruments/definition.py
+++ hijinks/materials/instruments/definition.py
@@ -6,15 +6,15 @@ instruments = abjad.InstrumentDictionary([
     (
         'piano',
         abjad.Piano(
-            name_markup=baca.markup.short_instrument('Pf.'),
-            short_name_markup=baca.markup.short_instrument('Pf.'),
+            markup=baca.markup.short_instrument('Pf.'),
+            short_markup=baca.markup.short_instrument('Pf.'),
             )
         ),
     (
         'violin',
         abjad.Violin(
-            name_markup=baca.markup.short_instrument('Vn.'),
-            short_name_markup=baca.markup.short_instrument('Vn.'),
+            markup=baca.markup.short_instrument('Vn.'),
+            short_markup=baca.markup.short_instrument('Vn.'),
             ),
         ),
     ])

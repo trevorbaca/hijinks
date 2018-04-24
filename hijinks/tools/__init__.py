9a6ca48242990a09fe80f9f4b376187eb1ab10bf
Trevor Bača <trevor.baca@gmail.com>
2018-04-24 17:54:47 -0500

    Cleaned up score.

--- hijinks/tools/__init__.py
+++ hijinks/tools/__init__.py
@@ -1,5 +1,4 @@
 from .ScoreTemplate import ScoreTemplate
-from .SegmentMaker import SegmentMaker
 from .margin_markup import margin_markup
 from .piano_rhythm import piano_rhythm
 from .split_pitches import split_pitches

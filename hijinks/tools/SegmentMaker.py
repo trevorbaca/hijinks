88fbd10b3cf90959585cd880da58f70f5ef8ca21
Trevor Bača <trevor.baca@gmail.com>
2018-04-24 15:30:18 -0500

    Cleaning up definition files.

--- hijinks/tools/SegmentMaker.py
+++ hijinks/tools/SegmentMaker.py
@@ -40,26 +40,6 @@ class SegmentMaker(abjad.SegmentMaker):
         violin_music_staff = score['ViolinMusicStaff']
         piano_staff_group = score['PianoStaffGroup']
 
-        #circuit = 8 * [0]
-        #circuit[1-1] = CC[5][111 - 1]
-        #circuit[2-1] = CC[6][68 - 1]
-        #circuit[3-1] = CC[7][31 - 1]
-        #circuit[4-1] = CC[0][175 - 1]
-        #circuit[5-1] = CC[1][93 - 1]
-        #circuit[6-1] = CC[2][31 - 1]
-        #circuit[7-1] = CC[3][2 - 1]
-        #circuit[8-1] = CC[4][85 - 1]
-
-        circuit = 8 * [0]
-        circuit[1-1] = [-37, -18, -16, -5, -3, 3, 13, 14, 16, 22, 24, 29]
-        circuit[2-1] = [-25, -17, -12, -10, -6, -4, -2, 5, 15, 16, 21, 25]
-        circuit[3-1] = [-36, -34, -26, -20, -16, -7, 3, 13, 23, 31, 42, 45]
-        circuit[4-1] = [10, 19, 20, 23, 24, 26, 27, 29, 30, 33, 37, 40]
-        circuit[5-1] = [-8, -4, -1, 5, 7, 12, 14, 15, 18, 21, 22, 25]
-        circuit[6-1] = [-20, -13, -12, -10, -2, 3, 8, 9, 13, 17, 19, 30]
-        circuit[7-1] = [-36, -32, -25, -22, -21, -14, -11, -4, -3, 5, 6, 7]
-        circuit[8-1] = [-34, -29, -26, -19, -12, -9, 1, 4, 11, 30, 32, 45]
-
         violin_tuplet_definitions = [
             ([4, 2, 2, 2], (8, 16)),
             ([2, 2, 4, 1, 1], (8, 16)),
@@ -114,7 +94,7 @@ class SegmentMaker(abjad.SegmentMaker):
         piano_rh_music_staff = score['PianoRHMusicStaff']
         rh_tuplets = []
         for rh_proportion, rh_pair, aggregate in zip(
-            rh_proportions, rh_pairs, circuit):
+            rh_proportions, rh_pairs, hijinks.circuit):
             rh_tuplet = tuplet_maker(rh_proportion, rh_pair)
             if isinstance(rh_tuplet, abjad.Tuplet):
                 duration = abjad.inspect(rh_tuplet).get_duration()
@@ -132,7 +112,7 @@ class SegmentMaker(abjad.SegmentMaker):
         piano_lh_music_staff = score['PianoLHMusicStaff']
         lh_tuplets = []
         for lh_proportion, lh_pair, aggregate in zip(
-            lh_proportions, lh_pairs, circuit):
+            lh_proportions, lh_pairs, hijinks.circuit):
             lh_tuplet = tuplet_maker(lh_proportion, lh_pair)
             if isinstance(lh_tuplet, abjad.Tuplet):
                 duration = abjad.inspect(lh_tuplet).get_duration()

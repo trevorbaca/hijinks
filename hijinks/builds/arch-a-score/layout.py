d2c43c720b14f13f2eb091d056b85a674ac8822b
Trevor Bača <trevor.baca@gmail.com>
2018-04-24 18:09:50 -0500

    Cleaned up score.

---
+++ hijinks/builds/arch-a-score/layout.py
@@ -0,0 +1,23 @@
+import baca
+
+
+breaks = baca.breaks(
+    baca.page(
+        baca.system(
+            4, 20, 22,
+            measure=1,
+            y_offset=40,
+            ),
+        baca.system(
+            4, 20, 22,
+            measure=6,
+            y_offset=100,
+            ),
+        baca.system(
+            4, 20, 22,
+            measure=11,
+            y_offset=167.5,
+            ),
+        number=1,
+        ),
+    )

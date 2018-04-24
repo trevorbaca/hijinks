3e05f5422e5d2d13a0011bdf5eb9aebbfb1afc56
Trevor Bača <trevor.baca@gmail.com>
2018-04-24 18:09:50 -0500

    Cleaned up score.

---
+++ hijinks/builds/arch-a-score/__metadata__.py
@@ -0,0 +1,14 @@
+import abjad
+
+
+metadata = abjad.OrderedDict(
+    [
+        (
+            'bol_measure_numbers',
+            [1, 6, 11],
+            ),
+        ('catalog_number_suffix', 'ARCH-A'),
+        ('paper_size', 'arch a'),
+        ('price', '$20'),
+        ]
+    )
---
+++ hijinks/builds/arch-a-score/stylesheet.ily
@@ -0,0 +1,9 @@
+#(ly:set-option 'relative-includes #t)
+\include "../../stylesheets/stylesheet.ily"
+
+#(set-default-paper-size "arch a")
+
+\paper {
+    left-margin = 24\mm
+    top-markup-spacing.minimum-distance = 8
+}

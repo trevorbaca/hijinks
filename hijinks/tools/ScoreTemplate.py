24a102ee2d2daf929c58ad05197a2b0909940cad
Trevor Bača <trevor.baca@gmail.com>
2018-08-06 23:20:50 -0500

    Tagged score template classes.

--- hijinks/tools/ScoreTemplate.py
+++ hijinks/tools/ScoreTemplate.py
@@ -19,49 +19,49 @@ class ScoreTemplate(baca.ScoreTemplate):
         ...     )
         >>> abjad.show(lilypond_file) # doctest: +SKIP
 
-        >>> abjad.f(lilypond_file[abjad.Score])
-        \context Score = "Score"
-        <<
-            \context GlobalContext = "GlobalContext" %! _make_global_context
-            <<                                       %! _make_global_context
-                \context GlobalRests = "GlobalRests" %! _make_global_context
-                {                                    %! _make_global_context
-                } %! _make_global_context
-                \context GlobalSkips = "GlobalSkips" %! _make_global_context
-                {                                    %! _make_global_context
-                } %! _make_global_context
-            >> %! _make_global_context
-            \context MusicContext = "MusicContext"
-            <<
-                \context ViolinMusicStaff = "ViolinMusicStaff"
-                {
-                    \context Voice = "ViolinMusicVoice"
-                    {
-                        \clef "treble" %! attach_defaults
-                        s1 %! ScoreTemplate.__illustrate__
-                    }
-                }
-                \context PianoStaffGroup = "PianoStaffGroup"
-                <<
-                    \context PianoRHMusicStaff = "PianoRHMusicStaff"
-                    {
-                        \context Voice = "PianoRHMusicVoice"
-                        {
-                            \clef "treble" %! attach_defaults
-                            s1 %! ScoreTemplate.__illustrate__
-                        }
-                    }
-                    \context PianoLHMusicStaff = "PianoLHMusicStaff"
-                    {
-                        \context Voice = "PianoLHMusicVoice"
-                        {
-                            \clef "treble" %! attach_defaults
-                            s1 %! ScoreTemplate.__illustrate__
-                        }
-                    }
-                >>
-            >>
-        >>
+        >>> abjad.f(lilypond_file[abjad.Score], strict=79)
+        \context Score = "Score"                                                       %! ScoreTemplate
+        <<                                                                             %! ScoreTemplate
+            \context GlobalContext = "GlobalContext"                                   %! _make_global_context
+            <<                                                                         %! _make_global_context
+                \context GlobalRests = "GlobalRests"                                   %! _make_global_context
+                {                                                                      %! _make_global_context
+                }                                                                      %! _make_global_context
+                \context GlobalSkips = "GlobalSkips"                                   %! _make_global_context
+                {                                                                      %! _make_global_context
+                }                                                                      %! _make_global_context
+            >>                                                                         %! _make_global_context
+            \context MusicContext = "MusicContext"                                     %! ScoreTemplate
+            <<                                                                         %! ScoreTemplate
+                \context ViolinMusicStaff = "ViolinMusicStaff"                         %! ScoreTemplate
+                {                                                                      %! ScoreTemplate
+                    \context Voice = "ViolinMusicVoice"                                %! ScoreTemplate
+                    {                                                                  %! ScoreTemplate
+                        \clef "treble"                                                 %! attach_defaults
+                        s1                                                             %! ScoreTemplate.__illustrate__
+                    }                                                                  %! ScoreTemplate
+                }                                                                      %! ScoreTemplate
+                \context PianoStaffGroup = "PianoStaffGroup"                           %! ScoreTemplate
+                <<                                                                     %! ScoreTemplate
+                    \context PianoRHMusicStaff = "PianoRHMusicStaff"                   %! ScoreTemplate
+                    {                                                                  %! ScoreTemplate
+                        \context Voice = "PianoRHMusicVoice"                           %! ScoreTemplate
+                        {                                                              %! ScoreTemplate
+                            \clef "treble"                                             %! attach_defaults
+                            s1                                                         %! ScoreTemplate.__illustrate__
+                        }                                                              %! ScoreTemplate
+                    }                                                                  %! ScoreTemplate
+                    \context PianoLHMusicStaff = "PianoLHMusicStaff"                   %! ScoreTemplate
+                    {                                                                  %! ScoreTemplate
+                        \context Voice = "PianoLHMusicVoice"                           %! ScoreTemplate
+                        {                                                              %! ScoreTemplate
+                            \clef "treble"                                             %! attach_defaults
+                            s1                                                         %! ScoreTemplate.__illustrate__
+                        }                                                              %! ScoreTemplate
+                    }                                                                  %! ScoreTemplate
+                >>                                                                     %! ScoreTemplate
+            >>                                                                         %! ScoreTemplate
+        >>                                                                             %! ScoreTemplate
 
     """
 
@@ -85,14 +85,16 @@ class ScoreTemplate(baca.ScoreTemplate):
         """
         Calls score template.
         """
+        tag = 'ScoreTemplate'
         # GLOBAL CONTEXT
         global_context = self._make_global_context()
 
         # VIOLIN
         violin_music_staff = abjad.Staff(
-            [abjad.Voice(name='ViolinMusicVoice')],
+            [abjad.Voice(name='ViolinMusicVoice', tag=tag)],
             lilypond_type='ViolinMusicStaff',
             name='ViolinMusicStaff',
+            tag=tag,
             )
         abjad.annotate(
             violin_music_staff,
@@ -107,19 +109,22 @@ class ScoreTemplate(baca.ScoreTemplate):
 
         # PIANO
         piano_rh_music_staff = abjad.Staff(
-            [abjad.Voice(name='PianoRHMusicVoice')],
+            [abjad.Voice(name='PianoRHMusicVoice', tag=tag)],
             lilypond_type='PianoRHMusicStaff',
             name='PianoRHMusicStaff',
+            tag=tag,
             )
         piano_lh_music_staff = abjad.Staff(
-            [abjad.Voice(name='PianoLHMusicVoice')],
+            [abjad.Voice(name='PianoLHMusicVoice', tag=tag)],
             lilypond_type='PianoLHMusicStaff',
             name='PianoLHMusicStaff',
+            tag=tag,
             )
         piano_staff_group = abjad.StaffGroup(
             [piano_rh_music_staff, piano_lh_music_staff],
             lilypond_type='PianoStaffGroup',
             name='PianoStaffGroup',
+            tag=tag,
             )
         abjad.annotate(
             piano_staff_group,
@@ -143,12 +148,14 @@ class ScoreTemplate(baca.ScoreTemplate):
             lilypond_type='MusicContext',
             is_simultaneous=True,
             name='MusicContext',
+            tag=tag,
             )
 
         # SCORE
         score = abjad.Score(
             [global_context, music_context],
             name='Score',
+            tag=tag,
             )
         self._assert_lilypond_identifiers(score)
         self._assert_unique_context_names(score)

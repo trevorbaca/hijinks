8bb16d94304ae937e0ff59be1dcb7fc16cf8a4fa
Trevor Bača <trevor.baca@gmail.com>
2019-02-19 15:18:39 -0600

    Retagged score template contributions.

--- hijinks/tools/ScoreTemplate.py
+++ hijinks/tools/ScoreTemplate.py
@@ -22,23 +22,23 @@ class ScoreTemplate(baca.ScoreTemplate):
         >>> abjad.f(lilypond_file[abjad.Score], strict=79)
         \context Score = "Score"                                                       %! ScoreTemplate
         <<                                                                             %! ScoreTemplate
-            \context GlobalContext = "Global_Context"                                  %! _make_global_context
-            <<                                                                         %! _make_global_context
-                \context GlobalRests = "Global_Rests"                                  %! _make_global_context
-                {                                                                      %! _make_global_context
-                }                                                                      %! _make_global_context
-                \context GlobalSkips = "Global_Skips"                                  %! _make_global_context
-                {                                                                      %! _make_global_context
-                }                                                                      %! _make_global_context
-            >>                                                                         %! _make_global_context
+            \context GlobalContext = "Global_Context"                                  %! abjad.ScoreTemplate._make_global_context
+            <<                                                                         %! abjad.ScoreTemplate._make_global_context
+                \context GlobalRests = "Global_Rests"                                  %! abjad.ScoreTemplate._make_global_context
+                {                                                                      %! abjad.ScoreTemplate._make_global_context
+                }                                                                      %! abjad.ScoreTemplate._make_global_context
+                \context GlobalSkips = "Global_Skips"                                  %! abjad.ScoreTemplate._make_global_context
+                {                                                                      %! abjad.ScoreTemplate._make_global_context
+                }                                                                      %! abjad.ScoreTemplate._make_global_context
+            >>                                                                         %! abjad.ScoreTemplate._make_global_context
             \context MusicContext = "Music_Context"                                    %! ScoreTemplate
             <<                                                                         %! ScoreTemplate
                 \context ViolinMusicStaff = "Violin_Music_Staff"                       %! ScoreTemplate
                 {                                                                      %! ScoreTemplate
                     \context Voice = "Violin_Music_Voice"                              %! ScoreTemplate
                     {                                                                  %! ScoreTemplate
-                        \clef "treble"                                                 %! attach_defaults
-                        s1                                                             %! ScoreTemplate.__illustrate__
+                        \clef "treble"                                                 %! abjad.ScoreTemplate.attach_defaults
+                        s1                                                             %! abjad.ScoreTemplate.__illustrate__
                     }                                                                  %! ScoreTemplate
                 }                                                                      %! ScoreTemplate
                 \context PianoStaffGroup = "Piano_Staff_Group"                         %! ScoreTemplate
@@ -47,16 +47,16 @@ class ScoreTemplate(baca.ScoreTemplate):
                     {                                                                  %! ScoreTemplate
                         \context Voice = "Piano_RH_Music_Voice"                        %! ScoreTemplate
                         {                                                              %! ScoreTemplate
-                            \clef "treble"                                             %! attach_defaults
-                            s1                                                         %! ScoreTemplate.__illustrate__
+                            \clef "treble"                                             %! abjad.ScoreTemplate.attach_defaults
+                            s1                                                         %! abjad.ScoreTemplate.__illustrate__
                         }                                                              %! ScoreTemplate
                     }                                                                  %! ScoreTemplate
                     \context PianoLHMusicStaff = "Piano_LH_Music_Staff"                %! ScoreTemplate
                     {                                                                  %! ScoreTemplate
                         \context Voice = "Piano_LH_Music_Voice"                        %! ScoreTemplate
                         {                                                              %! ScoreTemplate
-                            \clef "treble"                                             %! attach_defaults
-                            s1                                                         %! ScoreTemplate.__illustrate__
+                            \clef "treble"                                             %! abjad.ScoreTemplate.attach_defaults
+                            s1                                                         %! abjad.ScoreTemplate.__illustrate__
                         }                                                              %! ScoreTemplate
                     }                                                                  %! ScoreTemplate
                 >>                                                                     %! ScoreTemplate

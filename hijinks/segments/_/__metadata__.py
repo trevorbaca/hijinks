68c6d7824030286157387c6fdb41e5f3fe3b3a7f
Trevor Bača <trevor.baca@gmail.com>
2019-02-28 14:43:17 -0600

    Rerendered phantom measures.

--- hijinks/segments/_/__metadata__.py
+++ hijinks/segments/_/__metadata__.py
@@ -10,7 +10,6 @@ metadata = abjad.OrderedDict(
         ('duration', "0'31''"),
         ('final_measure_number', 15),
         ('first_measure_number', 1),
-        ('phantom', True),
         ('segment_name', '_'),
         ('segment_number', 1),
         ('start_clock_time', "0'00''"),

d5fa901c69cd51b6deb6c8a2200ae823f0f93f92
Trevor Bača <trevor.baca@gmail.com>
2019-09-20 17:27:44 -0400

    Cleaned up definition files.

--- hijinks/segments/_/definition.py
+++ hijinks/segments/_/definition.py
@@ -17,27 +17,27 @@ maker = baca.SegmentMaker(
     segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
     time_signatures=15*[(1, 8)],
     validate_measure_count=15,
-    )
+)
 
 maker(
     'Global_Skips',
     baca.metronome_mark('32'),
-    )
+)
 
 maker(
     'vn',
     baca.suite(
         hijinks.margin_markup('Vn.'),
         baca.start_markup('Violin', hcenter_in=10),
-        ),
-    )
+    ),
+)
 maker(
     'rh',
     baca.suite(
         hijinks.margin_markup('Pf.', context='PianoStaffGroup'),
         baca.start_markup('Piano', context='PianoStaffGroup', hcenter_in=10),
-        ),
-    )
+    ),
+)
 
 pp_sempre_al_fino = abjad.Markup.line([
     abjad.Markup('pp').dynamic(),
@@ -51,13 +51,13 @@ maker(
     baca.skeleton(hijinks.violin_rhythm(), tag=None),
     baca.staccato(selector=baca.notes().filter_duration('<=', (1, 16))),
     baca.tenuto(selector=baca.notes().filter_duration('>', (1, 16))),
-    )
+)
 
 maker(
     'rh',
     hijinks.piano_rhythm('rh'),
     baca.markup(pp_sempre_al_fino, direction=abjad.Down),
-    )
+)
 
 maker(
     'lh',
@@ -66,22 +66,22 @@ maker(
         abjad.Markup('ped. ad libitum').italic(),
         direction=abjad.Down,
         selector=baca.note(1),
-        ),
+    ),
     baca.text_script_padding(2),
     hijinks.piano_rhythm('lh'),
-    )
+)
 
 maker(
     (['rh', 'lh'], (1, -1)),
     baca.staccato(selector=baca.notes().filter_duration('<=', (1, 64))),
     baca.tenuto(selector=baca.notes().filter_duration('>', (1, 64))),
     baca.tuplet_bracket_shorten_pair((0, 0.6), selector=baca.top().tuplet(-1)),
-    )
+)
 
 maker(
     'lh',
     baca.literal(
         r"\override Score.SpanBar #'transparent = ##f",
         selector=baca.leaf(-1),
-        ),
-    )
+    ),
+)

03d757a1fb1af9cd0eeaba222785de4cb9b43762
Trevor Bača <trevor.baca@gmail.com>
2019-09-20 19:02:12 -0400

    Cleaned up definition files.

--- hijinks/segments/_/definition.py
+++ hijinks/segments/_/definition.py
@@ -10,7 +10,7 @@ import os
 
 maker = baca.SegmentMaker(
     check_all_are_pitched=True,
-    final_markup=(['Austin, TX.'], ['February 2006.']),
+    final_markup=(["Austin, TX."], ["February 2006."]),
     final_markup_extra_offset=(-9.5, -4),
     first_segment=True,
     final_segment=True,
@@ -20,66 +20,66 @@ maker = baca.SegmentMaker(
 )
 
 maker(
-    'Global_Skips',
-    baca.metronome_mark('32'),
+    "Global_Skips",
+    baca.metronome_mark("32"),
 )
 
 maker(
-    'vn',
+    "vn",
     baca.suite(
-        hijinks.margin_markup('Vn.'),
-        baca.start_markup('Violin', hcenter_in=10),
+        hijinks.margin_markup("Vn."),
+        baca.start_markup("Violin", hcenter_in=10),
     ),
 )
 maker(
-    'rh',
+    "rh",
     baca.suite(
-        hijinks.margin_markup('Pf.', context='PianoStaffGroup'),
-        baca.start_markup('Piano', context='PianoStaffGroup', hcenter_in=10),
+        hijinks.margin_markup("Pf.", context="PianoStaffGroup"),
+        baca.start_markup("Piano", context="PianoStaffGroup", hcenter_in=10),
     ),
 )
 
 pp_sempre_al_fino = abjad.Markup.line([
-    abjad.Markup('pp').dynamic(),
-    abjad.Markup('sempre al fino').italic(),
+    abjad.Markup("pp").dynamic(),
+    abjad.Markup("sempre al fino").italic(),
     ])
 
 maker(
-    'vn', 
+    "vn", 
     baca.markup(pp_sempre_al_fino, direction=abjad.Down),
     baca.pitches(hijinks.violin_pitches),
     baca.skeleton(hijinks.violin_rhythm(), tag=None),
-    baca.staccato(selector=baca.notes().filter_duration('<=', (1, 16))),
-    baca.tenuto(selector=baca.notes().filter_duration('>', (1, 16))),
+    baca.staccato(selector=baca.notes().filter_duration("<=", (1, 16))),
+    baca.tenuto(selector=baca.notes().filter_duration(">", (1, 16))),
 )
 
 maker(
-    'rh',
-    hijinks.piano_rhythm('rh'),
+    "rh",
+    hijinks.piano_rhythm("rh"),
     baca.markup(pp_sempre_al_fino, direction=abjad.Down),
 )
 
 maker(
-    'lh',
-    baca.clef('bass'),
+    "lh",
+    baca.clef("bass"),
     baca.markup(
-        abjad.Markup('ped. ad libitum').italic(),
+        abjad.Markup("ped. ad libitum").italic(),
         direction=abjad.Down,
         selector=baca.note(1),
     ),
     baca.text_script_padding(2),
-    hijinks.piano_rhythm('lh'),
+    hijinks.piano_rhythm("lh"),
 )
 
 maker(
-    (['rh', 'lh'], (1, -1)),
-    baca.staccato(selector=baca.notes().filter_duration('<=', (1, 64))),
-    baca.tenuto(selector=baca.notes().filter_duration('>', (1, 64))),
+    (["rh", "lh"], (1, -1)),
+    baca.staccato(selector=baca.notes().filter_duration("<=", (1, 64))),
+    baca.tenuto(selector=baca.notes().filter_duration(">", (1, 64))),
     baca.tuplet_bracket_shorten_pair((0, 0.6), selector=baca.top().tuplet(-1)),
 )
 
 maker(
-    'lh',
+    "lh",
     baca.literal(
         r"\override Score.SpanBar #'transparent = ##f",
         selector=baca.leaf(-1),

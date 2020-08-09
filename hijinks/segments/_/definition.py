fd8af309cd4330257f6c4ae9f18beeddb26c0bc3
Trevor Bača <trevor.baca@gmail.com>
2020-08-09 13:08:26 -0400

    Cleaned up path management.

--- hijinks/segments/_/definition.py
+++ hijinks/segments/_/definition.py
@@ -10,7 +10,7 @@ maker = baca.SegmentMaker(
     check_all_are_pitched=True,
     first_segment=True,
     final_segment=True,
-    segment_directory=abjad.Path(__file__).resolve().parent,
+    segment_directory=baca.Path(__file__).resolve().parent,
     time_signatures=15 * [(1, 8)],
     validate_measure_count=15,
 )

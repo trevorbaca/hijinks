7f6cf77e49a52d98e9786651ed94f07110221463
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 10:04:16 -0400

    Blacked code.

--- hijinks/builds/arch-a-score/__metadata__.py
+++ hijinks/builds/arch-a-score/__metadata__.py
@@ -4,11 +4,11 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [1, 6, 11],
-            ),
-        ('catalog_number_suffix', 'A'),
-        ('paper_size', 'arch a'),
-        ('price', '$20 / \\euro 20'),
-        ]
-    )
+        ),
+        ("catalog_number_suffix", "A"),
+        ("paper_size", "arch a"),
+        ("price", "$20 / \\euro 20"),
+    ]
+)

d2fad2f3654fb45faa36104458e36fd6e799202d
Trevor Bača <trevor.baca@gmail.com>
2021-04-28 09:50:59 -0400

    Cleaned up selectors.

--- hijinks/segments/01/definition.py
+++ hijinks/segments/01/definition.py
@@ -107,6 +107,6 @@ maker(
         baca.rehearsal_mark_extra_offset((-12, 6)),
         baca.rehearsal_mark_padding(0),
         baca.rehearsal_mark_self_alignment_x(abjad.Right),
-        selector=baca.leaves().rleak()[-1],
+        selector=baca.selectors.rleaf(-1),
     ),
 )

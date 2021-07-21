360e4dc407d0c23ea4899edef75f2823549faca2
Trevor Bača <trevor.baca@gmail.com>
2021-07-21 14:13:40 -0400

    Rerendered segments.

--- hijinks/segments/01/definition.py
+++ hijinks/segments/01/definition.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import abjad
 import baca
 
@@ -11,7 +13,7 @@ maker = baca.SegmentMaker(
     check_all_are_pitched=True,
     first_segment=True,
     final_segment=True,
-    segment_directory=baca.Path(__file__).resolve().parent,
+    segment_directory=pathlib.Path(__file__).resolve().parent,
     time_signatures=15 * [(1, 8)],
 )
 

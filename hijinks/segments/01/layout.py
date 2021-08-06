6f5f37cc407a6c85d3159f91fb14275bbf8c8f5e
Trevor Bača <trevor.baca@gmail.com>
2021-08-06 15:43:28 -0400

    Cleaned up layout files.

--- hijinks/segments/01/layout.py
+++ hijinks/segments/01/layout.py
@@ -2,21 +2,9 @@ import baca
 
 breaks = baca.breaks(
     baca.page(
-        baca.system(
-            (4, 20, 22),
-            measure=1,
-            y_offset=35,
-        ),
-        baca.system(
-            (4, 20, 22),
-            measure=6,
-            y_offset=85,
-        ),
-        baca.system(
-            (4, 20, 22),
-            measure=11,
-            y_offset=147.5,
-        ),
+        baca.system(measure=1, y_offset=35, distances=(4, 20, 22)),
+        baca.system(measure=6, y_offset=85, distances=(4, 20, 22)),
+        baca.system(measure=11, y_offset=147.5, distances=(4, 20, 22)),
         number=1,
     ),
 )

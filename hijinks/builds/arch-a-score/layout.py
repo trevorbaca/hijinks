3c96f03a373b577253a0a26a1f91fde0e36de84b
Trevor Bača <trevor.baca@gmail.com>
2021-08-06 15:43:28 -0400

    Cleaned up layout files.

--- hijinks/builds/arch-a-score/layout.py
+++ hijinks/builds/arch-a-score/layout.py
@@ -2,21 +2,9 @@ import baca
 
 breaks = baca.breaks(
     baca.page(
-        baca.system(
-            (4, 20, 22),
-            measure=1,
-            y_offset=40,
-        ),
-        baca.system(
-            (4, 20, 22),
-            measure=6,
-            y_offset=100,
-        ),
-        baca.system(
-            (4, 20, 22),
-            measure=11,
-            y_offset=167.5,
-        ),
+        baca.system(measure=1, y_offset=40, distances=(4, 20, 22)),
+        baca.system(measure=6, y_offset=100, distances=(4, 20, 22)),
+        baca.system(measure=11, y_offset=167.5, distances=(4, 20, 22)),
         number=1,
     ),
 )

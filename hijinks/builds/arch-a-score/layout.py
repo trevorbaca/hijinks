caa63080c59d4fded5dbe7ad34de6c3082fab476
Trevor Bača <trevor.baca@gmail.com>
2021-08-23 12:14:32 -0400

    Cleaned up layout files.

--- hijinks/builds/arch-a-score/layout.py
+++ hijinks/builds/arch-a-score/layout.py
@@ -3,10 +3,10 @@ import baca
 spacing = baca.SpacingSpecifier(
     breaks=baca.breaks(
         baca.page(
+            1,
             baca.system(measure=1, y_offset=40, distances=(4, 20, 22)),
             baca.system(measure=6, y_offset=100, distances=(4, 20, 22)),
             baca.system(measure=11, y_offset=167.5, distances=(4, 20, 22)),
-            number=1,
         ),
     ),
 )

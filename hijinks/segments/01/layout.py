d2a0a5a1e54c15f1529c6c73f047b5f0be6b7fc7
Trevor Bača <trevor.baca@gmail.com>
2021-08-23 12:14:32 -0400

    Cleaned up layout files.

--- hijinks/segments/01/layout.py
+++ hijinks/segments/01/layout.py
@@ -3,10 +3,10 @@ import baca
 spacing = baca.SpacingSpecifier(
     breaks=baca.breaks(
         baca.page(
+            1,
             baca.system(measure=1, y_offset=35, distances=(4, 20, 22)),
             baca.system(measure=6, y_offset=85, distances=(4, 20, 22)),
             baca.system(measure=11, y_offset=147.5, distances=(4, 20, 22)),
-            number=1,
         ),
     ),
 )

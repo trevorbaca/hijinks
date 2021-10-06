f224197f43935f4bf79238801b347c78271aec2e
Trevor Bača <trevor.baca@gmail.com>
2021-10-06 14:33:32 +0200

    Cleaned up definition files.

--- hijinks/segments/01/music.py
+++ hijinks/segments/01/music.py
@@ -117,23 +117,20 @@ commands(
 
 if __name__ == "__main__":
     keywords = baca.interpret.make_keyword_dictionary(
-        **baca.segment_interpretation_defaults(),
+        **baca.score_interpretation_defaults(),
         always_make_global_rests=True,
         error_on_not_yet_pitched=True,
         final_segment=True,
         global_rests_in_topmost_staff=True,
         score=score,
     )
-    lilypond_file_keywords = baca.make_lilypond_file_dictionary(
-        include_layout_ly=True,
-        includes=["../../stylesheet.ily"],
-    )
-    metadata, persist, score, timing = baca.build.interpret_segment_revised(
+    metadata, persist, score, timing = baca.build.interpret_segment(
         commands,
         **keywords,
     )
-    lilypond_file = baca.build.make_segment_lilypond_file(
+    lilypond_file = baca.make_lilypond_file(
         score,
-        lilypond_file_keywords=lilypond_file_keywords,
+        include_layout_ly=True,
+        includes=["../../stylesheet.ily"],
     )
-    baca.build.make_segment_pdf_revised(lilypond_file, metadata, persist, timing)
+    baca.build.make_segment_pdf(lilypond_file, metadata, persist, timing)

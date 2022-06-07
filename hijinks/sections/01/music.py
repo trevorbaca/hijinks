a060289368ce13592cf8a04c92240cb11b5fa750
Trevor Bača <trevor.baca@gmail.com>
2022-06-07 17:44:16 +0200

    Cleaned up music.py files.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -21,6 +21,7 @@ commands = baca.CommandAccumulator(
 
 baca.interpret.set_up_score(
     score,
+    commands,
     commands.manifests(),
     commands.time_signatures,
     always_make_global_rests=True,
@@ -32,9 +33,10 @@ manifests = commands.manifests()
 
 baca.commands._metronome_mark(skips[1 - 1], commands.metronome_marks["32"], manifests)
 
+baca.commands._bar_line(score["Skips"][15 - 1], "|.")
+
 commands(
     "Skips",
-    baca.bar_line("|.", lambda _: baca.select.skip(_, -1)),
     baca.literal(
         r"\override Score.BarLine.transparent = ##f",
         selector=lambda _: baca.select.skip(_, -1),

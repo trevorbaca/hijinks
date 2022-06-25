854a872f72dd77d575a9ec9ca86cc0d45c44c030
Trevor Bača <trevor.baca@gmail.com>
2022-06-25 11:58:02 -0400

    Cleaned up music.py files.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -31,7 +31,7 @@ baca.interpret.set_up_score(
 skips = score["Skips"]
 manifests = commands.manifests()
 
-baca.commands._metronome_mark(skips[1 - 1], commands.metronome_marks["32"], manifests)
+baca.metronome_mark(skips[1 - 1], commands.metronome_marks["32"], manifests)
 
 baca.bar_line(skips[15 - 1], "|.")
 

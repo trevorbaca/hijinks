59302e4f0ca861dcd89cb05785031a63a522596b
Trevor Bača <trevor.baca@gmail.com>
2022-07-17 16:20:23 -0400

    Cleaned up music.py files.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -48,124 +48,116 @@ baca.literal_function(
 )
 
 
-def _select_short_notes(argument):
-    result = abjad.select.notes(argument)
-    result = [_ for _ in result if _.written_duration <= abjad.Duration((1, 16))]
-    return result
+def VN(voice):
+    music = library.make_violin_rhythm()
+    voice.extend(music)
 
 
-def _select_long_notes(argument):
-    result = abjad.select.notes(argument)
-    result = [_ for _ in result if _.written_duration > abjad.Duration((1, 16))]
-    return result
+def PF(score):
+    voice = score["Piano.RH.Music"]
+    music = library.make_piano_material("rh", library.circuit())
+    voice.extend(music)
+    voice = score["Piano.LH.Music"]
+    music = library.make_piano_material("lh", library.circuit())
+    voice.extend(music)
 
 
-# VN
+def vn(voice):
+    def _select_short_notes(argument):
+        result = abjad.select.notes(argument)
+        result = [_ for _ in result if _.written_duration <= abjad.Duration((1, 16))]
+        return result
 
-voice = score["Violin.Music"]
+    def _select_long_notes(argument):
+        result = abjad.select.notes(argument)
+        result = [_ for _ in result if _.written_duration > abjad.Duration((1, 16))]
+        return result
 
-music = library.make_violin_rhythm()
-voice.extend(music)
-
-# RH
-
-voice = score["Piano.RH.Music"]
-
-music = library.make_piano_material("rh", library.circuit())
-voice.extend(music)
-
-# LH
-
-voice = score["Piano.LH.Music"]
-
-music = library.make_piano_material("lh", library.circuit())
-voice.extend(music)
-
-# vn
-
-accumulator(
-    "vn",
-    baca.instrument(accumulator.instruments["Violin"]),
-    baca.instrument_name(r"\hijinks-violin-markup"),
-    baca.clef("treble"),
-    library.short_instrument_name("Vn."),
-    baca.markup(
-        r"\hijinks-pp-sempre-al-fino-markup",
-        direction=abjad.DOWN,
-    ),
-    baca.pitches(library.violin_pitches()),
-    baca.staccato(selector=_select_short_notes),
-    baca.tenuto(selector=_select_long_notes),
-    baca.beam_positions(-4),
-)
-
-# rh
-
-accumulator(
-    "rh",
-    baca.instrument(accumulator.instruments["Piano"]),
-    baca.instrument_name(r"\hijinks-piano-markup", context="PianoStaff"),
-    library.short_instrument_name("Pf.", context="PianoStaff"),
-    baca.clef("treble"),
-    baca.markup(
-        r"\hijinks-pp-sempre-al-fino-markup",
-        direction=abjad.DOWN,
-    ),
-    baca.beam_positions(-6),
-    baca.stem_down(),
-)
-
-# lh
-
-accumulator(
-    "lh",
-    baca.clef("bass"),
-    baca.markup(
-        r"\hijinks-ped-ad-libitum-markup",
-        direction=abjad.DOWN,
-        selector=lambda _: abjad.select.note(_, 1),
-    ),
-    baca.text_script_padding(2),
-    baca.beam_positions(6),
-    baca.stem_up(),
-)
+    accumulator(
+        "vn",
+        baca.instrument(accumulator.instruments["Violin"]),
+        baca.instrument_name(r"\hijinks-violin-markup"),
+        baca.clef("treble"),
+        library.short_instrument_name("Vn."),
+        baca.markup(
+            r"\hijinks-pp-sempre-al-fino-markup",
+            direction=abjad.DOWN,
+        ),
+        baca.pitches(library.violin_pitches()),
+        baca.staccato(selector=_select_short_notes),
+        baca.tenuto(selector=_select_long_notes),
+        baca.beam_positions(-4),
+    )
 
 
-def _select_short_notes(argument):
-    result = abjad.select.notes(argument)
-    result = [_ for _ in result if _.written_duration <= abjad.Duration((1, 64))]
-    return result
+def pf(score):
+    accumulator(
+        "rh",
+        baca.instrument(accumulator.instruments["Piano"]),
+        baca.instrument_name(r"\hijinks-piano-markup", context="PianoStaff"),
+        library.short_instrument_name("Pf.", context="PianoStaff"),
+        baca.clef("treble"),
+        baca.markup(
+            r"\hijinks-pp-sempre-al-fino-markup",
+            direction=abjad.DOWN,
+        ),
+        baca.beam_positions(-6),
+        baca.stem_down(),
+    )
+    accumulator(
+        "lh",
+        baca.clef("bass"),
+        baca.markup(
+            r"\hijinks-ped-ad-libitum-markup",
+            direction=abjad.DOWN,
+            selector=lambda _: abjad.select.note(_, 1),
+        ),
+        baca.text_script_padding(2),
+        baca.beam_positions(6),
+        baca.stem_up(),
+    )
+
+    def _select_short_notes(argument):
+        result = abjad.select.notes(argument)
+        result = [_ for _ in result if _.written_duration <= abjad.Duration((1, 64))]
+        return result
+
+    def _select_long_notes(argument):
+        result = abjad.select.notes(argument)
+        result = [_ for _ in result if _.written_duration > abjad.Duration((1, 64))]
+        return result
+
+    accumulator(
+        ["rh", "lh"],
+        baca.staccato(selector=_select_short_notes),
+        baca.tenuto(selector=_select_long_notes),
+        baca.tuplet_bracket_shorten_pair(
+            (0, 0.6),
+            selector=lambda _: abjad.select.tuplet(abjad.select.top(_), -1),
+        ),
+    )
+    accumulator(
+        "lh",
+        baca.chunk(
+            baca.mark(r"\hijinks-colophon-markup"),
+            baca.rehearsal_mark_down(),
+            baca.rehearsal_mark_extra_offset((-7, -7)),
+            baca.rehearsal_mark_padding(0),
+            baca.rehearsal_mark_self_alignment_x(abjad.RIGHT),
+            selector=lambda _: abjad.select.leaf(_, -1),
+        ),
+    )
 
 
-def _select_long_notes(argument):
-    result = abjad.select.notes(argument)
-    result = [_ for _ in result if _.written_duration > abjad.Duration((1, 64))]
-    return result
+def main():
+    VN(accumulator.voice("vn"))
+    PF(score)
+    vn(score)
+    pf(score)
 
-
-accumulator(
-    ["rh", "lh"],
-    baca.staccato(selector=_select_short_notes),
-    baca.tenuto(selector=_select_long_notes),
-    baca.tuplet_bracket_shorten_pair(
-        (0, 0.6),
-        selector=lambda _: abjad.select.tuplet(abjad.select.top(_), -1),
-    ),
-)
-
-accumulator(
-    "lh",
-    baca.chunk(
-        baca.mark(r"\hijinks-colophon-markup"),
-        baca.rehearsal_mark_down(),
-        baca.rehearsal_mark_extra_offset((-7, -7)),
-        baca.rehearsal_mark_padding(0),
-        baca.rehearsal_mark_self_alignment_x(abjad.RIGHT),
-        selector=lambda _: abjad.select.leaf(_, -1),
-    ),
-)
 
 if __name__ == "__main__":
+    main()
     defaults = baca.interpret.section_defaults()
     del defaults["append_anchor_skip"]
     metadata, persist, score, timing = baca.build.section(

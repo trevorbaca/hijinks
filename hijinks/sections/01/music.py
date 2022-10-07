01e06b3e4b038099fe6a672cd4bfb8f918af7c5d
Trevor Bača <trevor.baca@gmail.com>
2022-10-07 08:44:06 -0400

    Cleaned up metadata.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -146,11 +146,9 @@ def make_score():
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
     score = make_score(environment.timing)
-    defaults = baca.section.section_defaults()
-    del defaults["append_anchor_skip"]
     metadata = baca.section.postprocess_score(
         score,
-        **defaults,
+        **baca.section.section_defaults(),
         always_make_global_rests=True,
         environment=environment,
         error_on_not_yet_pitched=True,

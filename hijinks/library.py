987574b4fd678016e54d4153be62d99d848adc91
Trevor Bača <trevor.baca@gmail.com>
2023-01-31 19:47:32 -0500

    Cleaned up ratio handling.

--- hijinks/library.py
+++ hijinks/library.py
@@ -69,24 +69,24 @@ def make_piano_material(staff, circuit):
     pairs["lh"] = [(n, 16) for n in (3, 4, 3, 2, 4, 4, 4, 4)]
     proportions = {}
     proportions["rh"] = [
-        abjad.Ratio((2, 2, 2, 1, 1, 1, 1)),
-        abjad.Ratio((1, 1, 4, 4, 4)),
-        abjad.Ratio((4, 4, 2, 2, 1, 1)),
-        abjad.Ratio((4, 1, 1, 1, 1, 4, 4, 2, 1, 1, 1, 1)),
-        abjad.Ratio((4, 2, 2, 2, 2, 1, 1, 1, 1)),
-        abjad.Ratio((4, 4, 1, 1, 4, 1, 1)),
-        abjad.Ratio((4, 12, 12)),
-        abjad.Ratio((1, 1, 2, 2, 4, 4)),
+        (2, 2, 2, 1, 1, 1, 1),
+        (1, 1, 4, 4, 4),
+        (4, 4, 2, 2, 1, 1),
+        (4, 1, 1, 1, 1, 4, 4, 2, 1, 1, 1, 1),
+        (4, 2, 2, 2, 2, 1, 1, 1, 1),
+        (4, 4, 1, 1, 4, 1, 1),
+        (4, 12, 12),
+        (1, 1, 2, 2, 4, 4),
     ]
     proportions["lh"] = [
-        abjad.Ratio((4, 3, 3, 3, 1)),
-        abjad.Ratio((4, 4, 3, 3, 2, 2, 2)),
-        abjad.Ratio((2, 2, 2, 4, 4, 4)),
-        abjad.Ratio((-8,)),
-        abjad.Ratio((6, 6, 8)),
-        abjad.Ratio((2, 2, 3, 3, 4)),
-        abjad.Ratio((2, 2, 1, 1, 1, 1, 4, 4, 4)),
-        abjad.Ratio((6, 6, 2, 2, 1, 1)),
+        (4, 3, 3, 3, 1),
+        (4, 4, 3, 3, 2, 2, 2),
+        (2, 2, 2, 4, 4, 4),
+        (-8,),
+        (6, 6, 8),
+        (2, 2, 3, 3, 4),
+        (2, 2, 1, 1, 1, 1, 4, 4, 4),
+        (6, 6, 2, 2, 1, 1),
     ]
     music = []
     for proportion, pair, aggregate in zip(proportions[staff], pairs[staff], circuit):

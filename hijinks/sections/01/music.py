d4b8cb6c397e76ecaf330d3345eef1d1124245c8
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:43 -0400

    Cleaned up music.py files.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -176,5 +176,17 @@ def main():
     )
 
 
+def make_layout():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=32, distances=(12, 26, 26)),
+            baca.system(measure=6, y_offset=98, distances=(4, 26, 26)),
+            baca.system(measure=11, y_offset=160, distances=(4, 26, 26)),
+        ),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()

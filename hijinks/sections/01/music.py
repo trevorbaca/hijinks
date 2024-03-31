b6d7b40c4a9b8a8caeb85648347d690c455bc028
Trevor Bača <trevor.baca@gmail.com>
2024-03-31 13:01:43 -0400

    Cleaned up music.py files.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -172,7 +172,7 @@ def persist_score(score, environment):
 
 
 def make_layout():
-    spacing = baca.make_layout(
+    layout = baca.make_layout(
         baca.page(
             1,
             baca.system(measure=1, y_offset=32, distances=(12, 26, 26)),
@@ -180,7 +180,7 @@ def make_layout():
             baca.system(measure=11, y_offset=160, distances=(4, 26, 26)),
         ),
     )
-    baca.section.make_layout_ly(spacing)
+    baca.section.make_layout_ly(layout)
 
 
 def main():

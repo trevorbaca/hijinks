1dc75f269d0faab2ccc9963f5a399c417e1e9146
Trevor Bača <trevor.baca@gmail.com>
2024-04-17 17:58:01 -0400

    Cleaned up music.py files.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -148,7 +148,6 @@ def persist_score(score, environment):
         score,
         environment,
         library.manifests,
-        error_on_not_yet_pitched=True,
         final_section=True,
         first_section=True,
         global_rests_in_topmost_staff=True,

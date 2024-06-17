7832d52eb96a0de02d7933a4adb8971448dd22ba
Trevor Bača <trevor.baca@gmail.com>
2024-06-17 14:42:18 -0400

    Cleaned up layout files.

--- hijinks/builds/arch-a-score/layout.py
+++ hijinks/builds/arch-a-score/layout.py
@@ -1,3 +1,6 @@
+import os
+import pathlib
+
 import baca
 
 
@@ -10,7 +13,10 @@ def main():
             baca.layout.System(11, y_offset=167.5, distances=(4, 20, 22)),
         ),
     )
-    baca.build.write_layout_ily(breaks)
+    build_directory = pathlib.Path(os.getcwd())
+    sections_directory = baca.path.get_contents_directory(build_directory) / "sections"
+    time_signatures = baca.build.accumulate_time_signatures(sections_directory)
+    baca.build.write_layout_ily(breaks, time_signatures)
 
 
 if __name__ == "__main__":

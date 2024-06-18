769c45532f8403f43992b4c71b1d650fab82278a
Trevor Bača <trevor.baca@gmail.com>
2024-06-18 13:46:17 -0400

    Cleaned up music.py files.

--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -179,7 +179,7 @@ def make_layout(environment):
             baca.layout.System(11, y_offset=160, distances=(4, 26, 26)),
         ),
     )
-    lilypond_file, bol_measure_numbers = baca.build.write_layout_ily(
+    return baca.build.write_layout_ily(
         breaks,
         environment.metadata["time_signatures"],
         fermata_measure_numbers=environment.metadata.get("fermata_measure_numbers", []),
@@ -194,7 +194,11 @@ def main():
         score = make_score(environment.timing)
         persist_score(score, environment)
     if environment.arguments.layout:
-        make_layout(environment)
+        lilypond_file, bol_measure_numbers = make_layout(environment)
+        baca.build.persist_layout_ily(environment.section_directory, lilypond_file)
+        baca.build.write_bol_metadata(
+            environment.section_directory, bol_measure_numbers
+        )
 
 
 if __name__ == "__main__":

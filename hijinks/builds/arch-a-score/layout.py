5bcca4af2057381713555c395b591f0b110736ec
Trevor Bača <trevor.baca@gmail.com>
2024-06-18 13:26:10 -0400

    Cleaned up layout.py files.

--- hijinks/builds/arch-a-score/layout.py
+++ hijinks/builds/arch-a-score/layout.py
@@ -1,10 +1,7 @@
-import os
-import pathlib
-
 import baca
 
 
-def main():
+def main(environment):
     breaks = baca.layout.Breaks(
         baca.layout.Page(
             1,
@@ -13,11 +10,14 @@ def main():
             baca.layout.System(11, y_offset=167.5, distances=(4, 20, 22)),
         ),
     )
-    build_directory = pathlib.Path(os.getcwd())
-    sections_directory = baca.path.get_contents_directory(build_directory) / "sections"
-    time_signatures = baca.build.accumulate_time_signatures(sections_directory)
-    baca.build.write_layout_ily(breaks, time_signatures)
+    return baca.build.write_layout_ily(
+        breaks,
+        environment.time_signatures,
+    )
 
 
 if __name__ == "__main__":
-    main()
+    environment = baca.build.read_build_directory_environment()
+    lilypond_file, bol_measure_numbers = main(environment)
+    baca.build.persist_layout_ily(environment.build_directory, lilypond_file)
+    baca.build.write_bol_metadata(environment.build_directory, bol_measure_numbers)
--- hijinks/sections/01/music.py
+++ hijinks/sections/01/music.py
@@ -179,7 +179,7 @@ def make_layout(environment):
             baca.layout.System(11, y_offset=160, distances=(4, 26, 26)),
         ),
     )
-    baca.build.write_layout_ily(
+    lilypond_file, bol_measure_numbers = baca.build.write_layout_ily(
         breaks,
         environment.metadata["time_signatures"],
         fermata_measure_numbers=environment.metadata.get("fermata_measure_numbers", []),
